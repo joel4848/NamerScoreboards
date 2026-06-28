aba22676bae51962cf71cfd8df6dd417d360032e
Joel Taylor <joel.w.taylor@gmail.com>
2026-06-28 12:52:40 +0100

    Changes mod name etc. to Nicknames Everywhere

--- src/main/java/com/joel4848/namerscoreboards/impl/NickSuggestionData.java
+++
@@ -1,7 +0,0 @@
-package com.joel4848.namerscoreboards.impl;
-
-import net.minecraft.text.Text;
-import org.jetbrains.annotations.Nullable;
-
-public record NickSuggestionData(Text name, @Nullable Text nick) {
-}
--- src/main/java/com/joel4848/namerscoreboards/pond/PlayerEntityDuck.java
+++
@@ -1,9 +0,0 @@
-package com.joel4848.namerscoreboards.pond;
-
-import net.minecraft.text.Text;
-
-public interface PlayerEntityDuck {
-    default Text namerscoreboards$getActualDisplayName() {
-        throw new UnsupportedOperationException("Implemented by Mixin");
-    }
-}
